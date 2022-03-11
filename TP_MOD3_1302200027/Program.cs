275a39edaf4c0d202e1d78b4f26ae5dcf3ca4341
ARZzxz <achmadrizky0211@gmail.com>
2022-03-11 16:44:25 +0700

    “menambahkan implementasi dengan table-driven”

--- TP_MOD3_1302200027/Program.cs
+++ TP_MOD3_1302200027/Program.cs
@@ -8,15 +8,49 @@ namespace TP_MOD3_1302200027
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
-        public void Main()
+        static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            kodePos table_Kodepos = new kodePos();
+            Console.WriteLine("=== GET KODE POS BATUNUNGGAL===");
+            table_Kodepos.getKodePos("Batununggal");
+            Console.WriteLine("==== GET ALL KODE POS ====");
+            table_Kodepos.getAllkodepos();
+        }
+    }
+    class kodePos
+    {
+        Dictionary<string, string> DIC = new Dictionary<string, string>()
+        {
+            {"Batununggal", "40266"},
+            {"Kujangsari" , "40287"},
+            {"Mengger"    , "40267"},
+            {"Wates"      , "40256"},
+            {"Cijaura"    , "40287"},
+            {"Jatisari"   , "40286"},
+            {"Margasari"  , "40286"},
+            {"Sekejati"   , "40286"},
+            {"Kebonwaru"  , "40272"},
+            {"Maleer"     , "40274"},
+            {"Samoja"     , "40273"} } ;
+        
+        public void getKodePos(string kel)
+        {
+            if (DIC.ContainsKey(kel))
+            { 
+                Console.WriteLine(kel + " : " + DIC[kel]); 
+            }
+            else
+            {
+                Console.WriteLine(kel + "Tidak ditemukan");
+            }
+        }
+        public void getAllkodepos()
+        {
+            foreach (KeyValuePair<string, string> elel in DIC)
+            {
+                Console.WriteLine("{0} \t\t {1}", elel.Key, elel.Value);
+            }
         }
     }
 }
